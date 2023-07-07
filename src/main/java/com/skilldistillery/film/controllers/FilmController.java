c480fe18a9b491f20b4fa4cc63043c757b0031e4
Jay Perkins <jul3jb@gmail.com>
2023-07-07 15:31:13 -0700

    Full-stack smooke test

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
 
@@ -10,4 +12,12 @@ public class FilmController {
 
 	@Autowired
 	private FilmDAO filmDao;
+
+	@RequestMapping(path = {"/", "home.do"})
+
+	public ModelAndView goHome() {
+		ModelAndView mv = new ModelAndView("WEB-INF/home.jsp");
+		return mv;
+	}
+
 }
