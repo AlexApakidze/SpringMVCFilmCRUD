5c07086ac48a3fe739f5f31acd623594753c0039
Jay Perkins <jul3jb@gmail.com>
2023-07-07 16:09:29 -0700

    Create showFilm.do, test form

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,11 +1,14 @@
 package com.skilldistillery.film.controllers;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -20,4 +23,15 @@ public class FilmController {
 		return mv;
 	}
 
+	@RequestMapping(path = "showFilm.do")
+	public ModelAndView showFilm(Integer filmId) throws SQLException {
+		ModelAndView mv = new ModelAndView("WEB-INF/home.jsp");
+		System.out.println("+++++++++++++ FilmId: " + filmId);
+		Film film = filmDao.findFilmById(filmId);
+		System.out.println("+++++++++++++ FilmId: " + filmId);
+		
+		mv.addObject("film", film);
+		return mv;
+		
+	}
 }
